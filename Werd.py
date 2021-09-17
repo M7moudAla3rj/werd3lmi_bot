232c83515053674bfbd3b320e52692438f083bc2
M7moudAla3rj <80818683+M7moudAla3rj@users.noreply.github.com>
2021-09-17 06:55:16 +0300

    Create Werd.py

---
+++ Werd.py
@@ -0,0 +1,33 @@
+import telebot
+
+import schedule
+
+import time
+
+bot=telebot.TeleBot("1967626202:AAFykDZY-cKrU7UmOCkBxqTKAb97RxbTKCg")
+
+i=0
+
+imege="imeges/"+str(i)+".jpg"
+
+chat_id="1202602736"
+
+def imeges ():
+
+  bot.send_message(chat_id , "ورد اليوم من الدراسة")
+
+  img=open (imege , 'rb')
+
+  bot.send_photo(chat_id, img)
+
+def i ():
+
+  i = i + 1
+
+schedule.every().day.at("06:32:00").do(imeges)
+
+schedule.every().day.at("17:30:01").do(i)
+
+while True:
+
+  schedule.run_pending()
